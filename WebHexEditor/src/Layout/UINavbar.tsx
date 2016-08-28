9c019b2e3be4571c297753b4545aed9fba45f09f
psrok1 <psrok1@gmail.com>
2016-08-28 22:45:44 +0200

    UINavbar, UIView

---
+++ WebHexEditor/src/Layout/UINavbar.tsx
@@ -0,0 +1,43 @@
+﻿import * as React from "react";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+
+interface UINavbarProps {
+    onSelect: (selectedItem: UINavbarItem, event?: React.SyntheticEvent) => any;
+}
+
+export enum UINavbarItem {
+    OpenFile,
+    Something
+}
+
+export class UINavbar extends React.Component<UINavbarProps, {}> {
+    render() {
+        return (
+            <Navbar inverse>
+                <Navbar.Header>
+                  <Navbar.Brand>
+                    <a href="#">WebHexEditor</a>
+                  </Navbar.Brand>
+                  <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav onSelect={this.props.onSelect}>
+                        <NavItem eventKey={UINavbarItem.OpenFile} href="#">Open file</NavItem>
+                        <NavItem eventKey={UINavbarItem.Something} href="#">Link</NavItem>
+                        <NavDropdown eventKey={UINavbarItem.Something} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={UINavbarItem.Something}>Action</MenuItem>
+                            <MenuItem eventKey={UINavbarItem.Something}>Another action</MenuItem>
+                            <MenuItem eventKey={UINavbarItem.Something}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={UINavbarItem.Something}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">Link Right</NavItem>
+                        <NavItem eventKey={2} href="#">Link Right</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+            )    
+    }
+}
---
+++ WebHexEditor/src/Layout/UIView.tsx
@@ -0,0 +1,51 @@
+﻿import * as React from "react";
+
+import { UINavbar, UINavbarItem } from "./UINavbar";
+import LoadModal from "./LoadModal";
+
+interface UIViewState {
+    loadModalOpened?: boolean;
+}
+
+export default class UIView extends React.Component<{}, UIViewState>
+{
+    constructor() {
+        super();
+        this.state = { loadModalOpened: false };
+    }
+
+    onNavbarSelect(eventKey: UINavbarItem, event?: React.SyntheticEvent) {
+        switch (eventKey) {
+            case UINavbarItem.OpenFile:
+                this.setState({ loadModalOpened: true });
+                break;
+            case UINavbarItem.Something:
+                alert("Something happened");
+                break;
+            default:
+                throw new Error("Selected unexpected UINavbarItem");
+        }
+    }
+
+    loadModalClose() {
+        this.setState({ loadModalOpened: false });
+    }
+
+    loadFile(file: File) {
+        this.loadModalClose();
+        /* ... loading file code */
+    }
+
+    render() {
+        return (
+            <section>
+                <UINavbar onSelect={ this.onNavbarSelect.bind(this) }/>
+                <div id="layout" />
+                <LoadModal
+                    showModal={ this.state.loadModalOpened }
+                    onCancel={ this.loadModalClose.bind(this) }
+                    onConfirm={ this.loadFile.bind(this) }/>
+            </section>
+        );
+    }
+}
